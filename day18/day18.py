1b3275827f171f5ee18fe17906eb7dd2fbabb704
BastienZim <bastien.zimmermann@imt-atlantique.net>
2021-12-24 16:10:13 +0100

    substantial advance - fixing explosion

--- day18/day18.py
+++ day18/day18.py
@@ -5,6 +5,7 @@ down = up
 
 '''
 import re
+from typing import Union
 
 def main():
     content = get_input(exBOOL = True)
@@ -19,7 +20,7 @@ def main():
 
 
 #---------------------Funcs--------------------
-def find_deep_nested_pairs(snail, verbose = False):
+def find_deep_nested_pairs(snail, verbose = True):
 
     depth = 0
     nested_deep = []#list containing 4 pairs or deeper pairs
@@ -33,7 +34,9 @@ def find_deep_nested_pairs(snail, verbose = False):
         if(verbose):
             print(f"  --  level: {depth} !")
             #print(snail)
-        new_indexes = find_higher_pairs(snail, pairs_indexes, verbose = False)
+        new_indexes = find_higher_pairs(snail, pairs_indexes, verbose = True)
+        new_pairs = [snail[beg:end] for beg,end in pairs_indexes]
+        print(f"   new_pairs: {new_pairs} !")
         if(depth>=3):
             pairs = [(snail[beg:end],beg,end) for beg,end in pairs_indexes]
             nested_deep.append((depth, pairs))
@@ -56,53 +59,19 @@ def snailfish(nums_a, nums_b, verbose = True):
     #1: Concat numbers
     added_nums = [nums_a + nums_b]
     snail = compact_str(added_nums)
-    snail = "[[[[1,3],[5,3]],[[1,3],[8,7]]],[[[4,9],[6,9]],[[8,2],[7,3]]]]"
+    #snail = "[[[[1,3],[5,3]],[[1,3],[8,7]]],[[[4,9],[6,9]],[[8,2],[7,3]]]]"
+    snail = "[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]"
     
     if(verbose): 
         print(f"Input : {nums_a} - { nums_b}")
         print(f"Snail : {snail}")
 
     #2 Find pairs nested inside 4 pairs and explode
-    nested_pairs = find_deep_nested_pairs(snail)
-    to_explode = nested_pairs[0][1][0][0]
-    ind_start = nested_pairs[0][1][0][1]
-    print(f"Nested pairs: {nested_pairs}")
-    
-    print()
-    print(f"To explode: {to_explode}")
-    #this might become 1 more [ symbol
-    leftmost_nested_pair = to_explode[to_explode.index("[[[[")+3:]
-    ind_start += 3
-    #print(f"Leftmost: {leftmost_nested_pair}")
-    count = 0
-    for i, x in enumerate(leftmost_nested_pair):
+    red_snail = explode(snail, verbose = False)
+    print(f"\nReduced exploded Snail is : {red_snail}")
 
-        #print(i,x, count)
-        if x == "[":   count += 1
-        elif x == "]": count -= 1
-        elif x == ',' and count == 0:
-            will_explode = eval(leftmost_nested_pair[:i])
-            print(f"Will explode: {will_explode}")
-            ind_end = ind_start + i
-            break
-        #elif(x == "]" and count == 0):
-        #    ind_expl_e = ind_start + i
-        #    break
-    pair_left = will_explode[0]
-    print("HERE",ind_start, ind_end, pair_left)
-
-    print("now----left")
-    if(number_to_left(snail[:ind_start])):
-        print("add to left number")
-    else:
-        print("do not add")
-    print("now----right")
-    if(number_to_right(snail[ind_end:])):
-        print("add to right number")
-    else:
-        print("do not add")
-        
-
+    #3 Find nums >= 10 and split !
+    spl_snail = is_10_or_greater(red_snail)
     #if (has_nested_pair(added_nums)):
     #    print("EXPLODE leftmost")
     #elif(is_10_or_greater(added_nums)):
@@ -113,43 +82,107 @@ def snailfish(nums_a, nums_b, verbose = True):
     #reduce
 
 
+def explode(snail, verbose = False):
+    nested_pairs = find_deep_nested_pairs(snail)
+    print(f"Nested pairs: {nested_pairs}")
+    if(len(nested_pairs)==0):
+        return(snail)
+    to_explode = nested_pairs[0][1][0][0]
+    ind_start = nested_pairs[0][1][0][1]
+
+    if(verbose):
+        print(f"Nested pairs: {nested_pairs}")
+        print()
+        print(f"To explode: {to_explode}")
+    #this might become 1 more [ symbol
+    leftmost_nested_pair = to_explode[to_explode.index("[[[[")+3:]
+    ind_start += 3
+    count = 0
+    for i, x in enumerate(leftmost_nested_pair):
+        if x == "[":   count += 1
+        elif x == "]": count -= 1
+        elif x == ',' and count == 0:
+            will_explode = eval(leftmost_nested_pair[:i])
+            #print(f"Will explode: {will_explode}")
+            ind_end = ind_start + i
+            break
+    
+    num_left, i_left = number_to_left(snail[:ind_start])
+    if(i_left > 0): num_left = str(int(num_left)+will_explode[0])
+    num_right, i_right = number_to_right(snail[ind_end:])
+    if(i_right > 0): num_right = str(int(num_right)+will_explode[1])
+    
+    patch = snail[ind_start-i_left : ind_end+i_right]
+    patch = num_left + snail[ind_start - i_left -len(num_left)+1 : ind_start]+\
+            snail[ind_end : ind_end + i_right -1] + num_right
+    
+    new_snail = snail[:ind_start-i_left] + patch + snail[ind_end+i_right:]
+    
+    if(verbose):
+        print("now----left")
+        print(f"num: {num_left}, index: {i_left}")
+        print("now----right")
+        print(f"num: {num_right}, index: {i_right}")
+        print(f"Patch    : {patch}")
+        print(f"Snail    : {snail}")
+        print(f"NEW Snail: {new_snail}")
+    return(new_snail)
+
+
 
 def number_to_left(snail):
-    print(snail)
-    print(snail.split())
-    extracted_nums = [s for s in snail.split() if s.isdigit()]
-    print(extracted_nums)
+    #print(snail)
+    #print(snail.split())
+    extracted_nums = [s for s in re.findall(r'\b\d+\b', snail)]
+    #print(extracted_nums)
     if(len(extracted_nums)>1):
-        print(extracted_nums[-1])
+        number = extracted_nums[-1]
+        
+        index = snail[::-1].index(number)
+        return((number, index))
     else:
-        return("")
-    print()      
-    return(True)
+        return(("0",0))
 
 def number_to_right(snail):
-    print(snail)
-    print(snail.split())
-    extracted_nums = [s for s in snail.split() if s.isdigit()]
-    print(extracted_nums)
+    #print(snail)
+    extracted_nums = [str(s) for s in re.findall(r'\b\d+\b', snail)]
+    #print(extracted_nums)
     if(len(extracted_nums)>0):
-        print(extracted_nums[-1])
-    print()     
-    return(True)
+        number = extracted_nums[0]
+        index = snail.index(number)+len(str(number))
+        #print(f"num: {number}, index: {index}")
+        return((number, index))
+    else:
+        return(("0",0))
 
 
 def find_higher_pairs(snail, pairs_indexes, verbose = False):
     pairs = [(snail[beg:end],beg,end) for beg,end in pairs_indexes]
-
-    if(verbose): print([x[0] for x in pairs])
+    elts =  [(s) for s in re.findall(r'\b\d+\b', snail)]
+    elts_i =  [(s.start(),s.end()) for s in re.finditer(r'\b\d+\b', snail)]
+    elts_w_i = [(x, s[0], s[1]) for x, s in zip(elts, elts_i)]
+    for p, pb, pe in pairs:
+        elts_w_i = list(filter(lambda x: (x[1]<pb or x[1]>pe), elts_w_i))
+        elts_w_i = list(filter(lambda x: (x[2]<pb or x[2]>pe), elts_w_i))
+    #print(f" ### ELTS {elts_w_i}")
+    #print(f" ### Pairs {pairs}")
+    
+    print("\n\n" + "----"*19)
+    pair_elts = sorted(elts_w_i+pairs, key = lambda x: x[1])
+    for x in pair_elts:
+        is_pair(x[0])
+    print("\n\n" + "####"*19)
+    
+    if(verbose): print([x[0] for x in pair_elts])
     new_indexes = []
     i = 0
-    if(len(pairs)==1):
+    if(len(pair_elts)==1):
         return(pairs_indexes)
 
 
-    while(i<len(pairs)-1):
-        p_1, b_1, e_1 = pairs[ i ]
-        p_2, b_2, e_2 = pairs[ i + 1 ]
+    while(i<len(pair_elts)-1):
+        p_1, b_1, e_1 = pair_elts[ i ]
+        p_2, b_2, e_2 = pair_elts[ i + 1 ]
         p_pair = "[" + p_1 + "," + p_2 + "]"
         ref = snail[b_1-1 : e_2+1]
         if(verbose): print("\n",p_pair, ref)
@@ -159,7 +192,7 @@ def find_higher_pairs(snail, pairs_indexes, verbose = False):
             i+=2
         else:
             if(verbose): print("NO", p_1, p_2, "_-_", p_pair, ref)
-            if(i <= len(pairs)-2):
+            if(i <= len(pair_elts)-2):
                 #print("NO",p_1)
                 new_indexes.append((b_1,e_1))
             else:
@@ -172,6 +205,11 @@ def find_higher_pairs(snail, pairs_indexes, verbose = False):
 
     #print(left, snail[:beg])
         #extend_left:
+def is_pair(p_pair):
+    if(p_pair.count("[") == p_pair.count("]") and p_pair.count("[") > 0):
+        return(True)
+    else:
+       return(False)
 
 
 def find_number_pairs(snail, verbose = False):
@@ -216,12 +254,12 @@ def find_number_pairs(snail, verbose = False):
             print(f"   pair: {snail[a:b]} - start at {a} end at {b}")
     return(pairs_indexes)
     
-
+"""
 def find_2_digits_nums(snail):
     extracted_nums = [s for s in potential_pair.split() if s.isdigit() if len(s)>=2]
     print(snail.index(s))
 
-
+"""
 def compact_str(snail):
     if(type(snail) != str):
         snail = str(snail)
@@ -230,22 +268,38 @@ def compact_str(snail):
     snail = snail.replace(" ","")
     return(snail)
 
-
-def explode(pair):
-    expl = pair
-    p_sum = sum(pair)
-
+        
     
-    return(expl)
 
+'''
 def has_nested_pair(numbers):
     """If any pair is nested inside four pairs, the leftmost such pair explodes."""
-    return(True)
+    return(True)'''
 
 
-def is_10_or_greater(numbers):
+def is_10_or_greater(snail, verbose=False):
     """If any regular number is 10 or greater, the leftmost such regular number splits"""
-    return(True)
+    dd_num = [int(s) for s in re.findall(r'\b\d{2}\b', snail)]
+    if(len(dd_num)!=0): 
+        dd_num = dd_num[0]
+    else: 
+        return(snail)
+    dd_i = [(s.start(),s.end()) for s in re.finditer(r'\b\d{2}\b', snail)][0]
+    if(verbose): print(dd_num, dd_i)
+    if((dd_num/2)%1 !=0):
+        left_val = int(dd_num/2)
+        right_val = int(dd_num/2)+1
+    else:
+        left_val = int(dd_num/2)
+        right_val = int(dd_num/2)
+        
+    patch = str([left_val,right_val])
+    new_snail = snail[:dd_i[0]] + patch + snail[dd_i[1]:]
+    if(verbose):
+        print(f"Patch    : {patch}")
+        print(f"Snail    : {snail}")
+        print(f"NEW Snail: {new_snail}")
+    return(new_snail)
 
 #-------------INPUT--------------------------
 def get_input(exBOOL = False):
