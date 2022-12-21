17605472ce7453a4019f51ad077db34d9a615b0e
BastienZim <bastien.zimmermann@imt-atlantique.net>
2022-12-21 12:01:44 +0100

    Day 14 Done

--- 2022/day14/day14.py
+++ 2022/day14/day14.py
@@ -11,8 +11,15 @@ path = "/home/bastienzim/Documents/perso/adventOfCode/2022"
 #path = "/home/bastien/Documents/AdventOfCode/2022"
 
 
-example = True
+example = False
 
+
+if(example):
+    source_point = (16, 0)
+    global_max_depth = 11
+else:
+    source_point = (193, 0)
+    global_max_depth = 184
 
 def main():
     day = str(os.path.basename(__file__).split(".")[0][3:])
@@ -26,7 +33,7 @@ def main():
     input = [[tuple(int(a) for a in l.split(","))
               for l in x.split(" -> ")] for x in input]
     # print(input)
-    
+
     nodes_to_draw = []
     for x in input:
         for b, a in zip(x, x[1:]):
@@ -40,129 +47,103 @@ def main():
                 print('diag')
 
     nodes_to_draw = list(set(nodes_to_draw))
-    print("MAX_DEPTH =",max([x[1] for x in nodes_to_draw])+2)
-    sand = []
-    map = {n: "rocks" for n in nodes_to_draw}
-    map = [n for n in nodes_to_draw]
-    print()
-
-    '''
-    last_sand_len = len(map)
-    for i in range(10000):
-        #sand = update_sand(sand, nodes_to_draw)
-        map = update_sand_map(map)
-        if(last_sand_len == len(map)):
-            print("ENDING")
-            break
-        else:
-            last_sand_len=len(map)
-        if(i%500 == 0):
-            print(i)
-
-            draw_rocks_map(nodes_to_draw, map, input)
-            
-    #sand.append((500,0))
-    map.append((500,0))'''
 
     max_depth = max([x[1] for x in nodes_to_draw])+2
     max_width = 1+2*(max_depth-1)
-    g_map = np.zeros((max_depth, max_width), dtype=bool)
-
-    centered_nodes = [(a[0]-min([x[0] for x in nodes_to_draw]), a[1]) for a in nodes_to_draw]
+    print(f"max_depth: {max_depth} - max_width: {max_width} - > center at {max_width/2} -> Source {500-min([x[0] for x in nodes_to_draw])+int(max_width/2)}")
+    
+    g_map = np.zeros((max_width+int(max_width/2), max_depth), dtype=bool)
+    centered_nodes = [(a[0]-min([x[0] for x in nodes_to_draw])+int(max_width/2), a[1])
+                      for a in nodes_to_draw]
     for n in centered_nodes:
         g_map[n] = 1
-    #print(np.array(g_map, dtype=int))
 
-    #First result = 7.78 for 1000
+    i_final, map = propagate_sand(g_map)
+
+    #print(np.array(map.T, dtype=int))
+    
     #n_iter = 1000
-    #time = timeit.timeit(lambda: propagate_sand(centered_nodes), number = n_iter)
+    #time = timeit.timeit(lambda: propagate_sand(g_map), number=n_iter)
     #print(time)
-    
-    
+
     #i,map = propagate_sand(centered_nodes)
-    #print(f"===> n_steps: {i, len(map)-len(nodes_to_draw)} <===")
-    #draw_rocks_map(centered_nodes, map)
+    print(f"===> n_steps: {i_final, np.sum(map)-len(centered_nodes)} <===")
+
+    res_nodes = []
+    for i in range(map.shape[0]):
+        for j in range(map.shape[1]):
+            if(map[(i, j)] == 1):
+                res_nodes.append((i, j))
+
+    print(np.sum(map)-len(centered_nodes))
+    draw_rocks_map(centered_nodes, res_nodes)
     #draw_rocks(nodes_to_draw, sand, input)
 
 
-
-def propagate_sand(nodes_to_draw):
-    map = [n for n in nodes_to_draw]
-    last_sand_len = len(map)
-    for i in range(10000):
-        #sand = update_sand(sand, nodes_to_draw)
-        map = update_sand_map(map)
-        if(last_sand_len == len(map)):
-            #print("ENDING")
+def propagate_sand(g_map):
+    last_sand_len = np.sum(g_map)
+    for i in range(1000000):
+        g_map = update_sand_map(g_map)
+        if(last_sand_len == np.sum(g_map)):
             break
         else:
-            last_sand_len=len(map)
-        #if(i%500 == 0):
-        #    print(i)
-    map.append((6,0))
-    return(i,map)
+            last_sand_len = np.sum(g_map)
+        if(i%1000 == 0):
+            print(i)
+    g_map[source_point] = 1
+    return(i, g_map)
+
 
 def update_sand(sand_in, rocks):
-    can_move, move =  next_move((6, 0), sand_in, rocks)#(500, 0) is sand source
+    can_move, move = next_move((6, 0), sand_in, rocks)
     if(not can_move):
-        #print("Filled up")
         return(sand_in)
 
     sand = sand_in.copy()
     while(can_move):
         point = move
         can_move, move = next_move(point, sand, rocks)
-    if(move != "STOP"):
-        sand.append(point)
+    sand.append(point)
     return(sand)
 
-def update_sand_map(map_in):
-    can_move, move =  next_move_map((6, 0), map_in)#(500, 0) is sand source
-    if(not can_move):
-        #print("Filled up")
-        return(map_in)
 
-    map = map_in.copy()
+def update_sand_map(g_map):
+    can_move, move = next_move_map(source_point, g_map)  # (500, 0) is sand source
+    if(not can_move):
+        return(g_map)
     while(can_move):
         point = move
-        can_move, move = next_move_map(point, map_in)
-    if(move != "STOP"):
-        #print(point)
-        map.append(point)
-        
-    return(map)
+        can_move, move = next_move_map(point, g_map)
+    g_map[point] = 1
+    return(g_map)
+
 
 def next_move(point, sand, rocks):
-    #if(point[1] > max([x[1] for x in rocks])+2):#if reached bellow bottom.
-    #    return(False, "STOP")
     bellow = tuple(np.add(point, (0, 1)))
     left = tuple(np.add(bellow, (-1, 0)))
     right = tuple(np.add(bellow, (1, 0)))
     for test in [bellow, left, right]:
-        type_test= get_type(test, sand, rocks)
+        type_test = get_type(test, sand, rocks)
         if(type_test == "nothing"):
             return(True, test)
     else:
         return(False, None)
 
-#example
-def next_move_map(point, map):
-    bellow = tuple(np.add(point, (0, 1)))
-    if(bellow[1] >= 11):#184
+
+def next_move_map(point, g_map):
+    test = (point[0], point[1]+1)
+    if(test[1] >= global_max_depth):  # 184
         return(False, None)
-    if(bellow not in map):
-        return(True, bellow)
-    test = tuple(np.add(bellow, (-1, 0)))
-    if(test not in map):
+    if(g_map[test] == 0):
         return(True, test)
-    test = tuple(np.add(bellow, (1, 0)))
-    if(test not in map):
+    test = (point[0]-1, point[1]+1)
+    if(g_map[test] == 0):
         return(True, test)
-    
+    test = (point[0]+1, point[1]+1)
+    if(g_map[test] == 0):
+        return(True, test)
     return(False, None)
 
-#def get_type(point, map):
-#    return(map[point])
 
 def get_type(point, sand, rocks):
     if(len(rocks) > 0):
@@ -204,13 +185,11 @@ def draw_rocks_map(nodes_to_draw, map):
 
 def draw_rocks(nodes_to_draw, sand):
     rocks = []
-    sand_source = [(6, 0)]
-    
     rocks = np.array(list(set(nodes_to_draw)))
     sand = np.array(list(set(sand)))
-    min_x = min(np.min(rocks[:, 0]),np.min(sand[:, 0]))
-    max_x = max(np.max(rocks[:, 0]),np.max(sand[:, 0]))
-    max_y = max(np.max(rocks[:, 1]),np.max(sand[:, 1]))
+    min_x = min(np.min(rocks[:, 0]), np.min(sand[:, 0]))
+    max_x = max(np.max(rocks[:, 0]), np.max(sand[:, 0]))
+    max_y = max(np.max(rocks[:, 1]), np.max(sand[:, 1]))
     rocks = [tuple(x) for x in rocks]
     sand = [tuple(x) for x in sand]
     map = []
@@ -222,7 +201,7 @@ def draw_rocks(nodes_to_draw, sand):
                 row.append("#")
             elif((i, j) in sand):
                 row.append("o")
-            elif((i, j) in sand_source):
+            elif((i, j) in source_point):
                 row.append("+")
             else:
                 row.append(".")
